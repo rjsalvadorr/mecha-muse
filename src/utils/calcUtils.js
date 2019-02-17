3d7e4dca759b4327b76f9af0353cafa87ddf20bb
RJ Salvador <rjsalvadorr@gmail.com>
2019-02-17 09:25:46 -0500

    Updated documentation again

--- src/utils/calcUtils.js
+++ src/utils/calcUtils.js
@@ -5,36 +5,39 @@ class CalcUtils {
 
     /**
      * Returns a random integer between the two given numbers
+     * @static
      * @param {number} minimum - minimum number (inclusive)
      * @param {number} maximum - maximum number (exclusive)
      * @returns {number} - random integer between the two parameters
      */
-  static getRandomInt = (minimum, maximum) => {
+  static getRandomInt(minimum, maximum) {
     const min = Math.ceil(minimum);
     const max = Math.floor(maximum);
     return Math.floor(Math.random() * (max - min)) + min;
     // The maximum is exclusive and the minimum is inclusive
-  };
+  }
 
     /**
      * Finds the distance between two pitches, given in semitones
+     * @static
      * @param {string} pitch1 - first pitch
      * @param {string} pitch2 - second pitch
      * @returns {number} - distance between pitches, in semitones
      */
-  static getPitchDistance = (pitch1, pitch2) => {
+  static getPitchDistance(pitch1, pitch2) {
     // Stubbed for now
     const placeholder = pitch1 + pitch2;
     return placeholder ? 0 : 0;
-  };
+  }
 
     /**
      * Splits an integer into several random parts
+     * @static
      * @param {number} numToSplit - integer to split
      * @param {number} numSections - # of sections
-     * @returns {(number|Array)} - series of random
+     * @returns {number[]} - series of random
      */
-  static splitInteger = (numToSplit, numSections) => {
+  static splitInteger(numToSplit, numSections) {
     const splitValues = [];
     let remaining = numToSplit;
     let nextNumber;
@@ -45,7 +48,7 @@ class CalcUtils {
     }
     splitValues.push(remaining);
     return splitValues;
-  };
+  }
 }
 
 export default CalcUtils;
