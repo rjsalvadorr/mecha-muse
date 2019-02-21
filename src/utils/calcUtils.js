2fa73add3f89656b1e7f7f7c3b82f6d8b55bb61c
Randolph Salvador <randolph.salvador@intelex.com>
2019-02-21 12:31:06 -0500

    Updated documentation

--- src/utils/calcUtils.js
+++ src/utils/calcUtils.js
@@ -2,12 +2,12 @@
  * Utility functions for numerical/math calculations
  */
 class CalcUtils {
-  /**
+    /**
      * Returns a random integer between the two given numbers
      * @static
      * @param {number} minimum - minimum number (inclusive)
      * @param {number} maximum - maximum number (exclusive)
-     * @returns {number} - random integer between the two parameters
+     * @returns {number} random integer between the two parameters
      */
   static getRandomInt(minimum, maximum) {
     const min = Math.ceil(minimum);
@@ -16,25 +16,12 @@ class CalcUtils {
     // The maximum is exclusive and the minimum is inclusive
   }
 
-  /**
-     * Finds the distance between two pitches, given in semitones
-     * @static
-     * @param {string} pitch1 - first pitch
-     * @param {string} pitch2 - second pitch
-     * @returns {number} - distance between pitches, in semitones
-     */
-  static getPitchDistance(pitch1, pitch2) {
-    // Stubbed for now
-    const placeholder = pitch1 + pitch2;
-    return placeholder ? 0 : 0;
-  }
-
-  /**
+    /**
      * Splits an integer into several random parts
      * @static
      * @param {number} numToSplit - integer to split
      * @param {number} numSections - # of sections
-     * @returns {number[]} - series of random
+     * @returns {number[]} addends
      */
   static splitInteger(numToSplit, numSections) {
     const splitValues = [];
